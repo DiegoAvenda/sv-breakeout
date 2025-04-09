6f42fb5518d25a97b879855a1b8112ba9143090c
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2025-04-09 16:04:33 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	let canvas;
+
+	$effect(() => {
+		const ctx = canvas.getContext('2d');
+		ctx.strokeRect(0, 0, canvas.width, canvas.height);
+	});
+</script>
+
+<h1>hi</h1>
+
+<canvas bind:this={canvas}></canvas>
