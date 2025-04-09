b69587fd0c42e444a3b7edc6cd0a1676c653b896
Diego Avendaño <75541249+DiegoAvenda@users.noreply.github.com>
2025-04-09 17:16:34 -0600

    paddle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,53 @@
 
 	$effect(() => {
 		const ctx = canvas.getContext('2d');
-		ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+		let ballX = canvas.width / 2;
+		let ballY = canvas.height / 2;
+		let ballDX = 2;
+		let ballDY = -2;
+		let ballRadius = 10;
+		const paddleHeight = 10;
+		const paddleWidth = 75;
+		let paddleX = (canvas.width - paddleWidth) / 2;
+
+		function drawBall() {
+			ctx.beginPath();
+			ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+			ctx.fillStyle = 'red';
+			ctx.fill();
+			ctx.closePath();
+		}
+
+		function drawPaddle() {
+			ctx.beginPath();
+			ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+			ctx.fillStyle = 'blue';
+			ctx.fill();
+		}
+
+		function draw() {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+			ctx.strokeRect(0, 0, canvas.width, canvas.height);
+			drawBall();
+			drawPaddle();
+
+			if (ballY < ballRadius) {
+				ballDY = -ballDY;
+			}
+
+			if (ballX > canvas.width - ballRadius || ballX < ballRadius) {
+				ballDX = -ballDX;
+			}
+
+			ballX += ballDX;
+			ballY += ballDY;
+
+			requestAnimationFrame(draw);
+		}
+
+		draw();
 	});
 </script>
 
